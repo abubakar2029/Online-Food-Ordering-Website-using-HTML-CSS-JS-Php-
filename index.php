e007bcf25ba1163bda3479a6f3747d45ca799bf3
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2024-12-25 11:45:50 +0500

    Added tailwind css, removed reservation component as it is not required

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Food App</title>
-</head>
+<?php include './includes/head.php'?>
 <body>
     <?php include 'components/header.php'?>
     <h1>Welcome to the shop!</h1>
-
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     <?php include 'components/footer.php';?>
 
     <div>
         <?php
-        include 'components/menu.php';
-        ?>
+// include 'components/menu.php';
+?>
 
     </div>
 </body>
