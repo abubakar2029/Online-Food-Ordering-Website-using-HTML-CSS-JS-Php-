9a9ef823312ce5b3d702e7a599a80116baf1e768
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-01-16 14:39:42 +0500

    A bug in navbar removed

--- components/navbar.php
+++ components/navbar.php
@@ -1,10 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+<div>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <a class="navbar-brand fw-bold fs-4" href="#">
   <img src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Logo" height="40" class="me-2">
-    
+
   Navbar</a>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
@@ -22,3 +24,4 @@
       <a href="signup.php" class="btn btn-outline-success my-2 my-sm-0" type="submit">Signup/Login</a>
   </div>
 </nav>
+</div>
