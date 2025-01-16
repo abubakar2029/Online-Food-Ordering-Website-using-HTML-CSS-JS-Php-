b4a0e26db51570475a4b2d605651eaff17243177
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-01-16 14:37:09 +0500

    Navbar modified

--- components/navbar.php
+++ components/navbar.php
@@ -1,41 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">
-      <img src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Logo" height="30">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="addItem.php">Add Food Item</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Create Account</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Log Out</a>
-        </li>
-      </ul>
-      <div class="d-flex align-items-center">
-        <button class="btn btn-dark" type="button">
-          <i class="fas fa-bell"></i>
-        </button>
-        <div class="dropdown ms-3">
-          <button class="btn btn-dark dropdown-toggle" type="button" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
-            <img class="rounded-circle" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="User Profile" width="30">
-          </button>
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userMenu">
-            <li><a class="dropdown-item" href="#">Your Profile</a></li>
-            <li><a class="dropdown-item" href="signOut">Sign Out</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand fw-bold fs-4" href="#">
+  <img src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Logo" height="40" class="me-2">
+    
+  Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+      <a href="signup.php" class="btn btn-outline-success my-2 my-sm-0" type="submit">Signup/Login</a>
+  </div>
 </nav>
